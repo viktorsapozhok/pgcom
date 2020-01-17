8b807fbce7ca93bc139fbd74c3e451f8c81e6cf3
alex <piskun.aleksey@gmail.com>
2020-01-17 23:57:31 +0100

    exception handler if module not found

--- pgcom/__init__.py
+++ pgcom/__init__.py
@@ -1,3 +1,7 @@
-from .commuter import *  # noqa: F401, F403
-from .listener import *  # noqa: F401, F403
-from .version import __version__
+try:
+    from .commuter import *  # noqa: F401, F403
+    from .listener import *  # noqa: F401, F403
+except ModuleNotFoundError:
+    pass
+
+from .version import __version__  # noqa: F401
