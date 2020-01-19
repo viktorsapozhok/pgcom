d53e11c78f41aa575d83bdbde184bad6fcc560d5
alex <piskun.aleksey@gmail.com>
2020-01-19 21:46:25 +0100

    docs updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 import sphinx_rtd_theme
 import pgcom
-
-sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
